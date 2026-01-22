a997bed213ab1fe132eec448bf0ba2b183d72602
Katelyn Petersen <katelynpetersen@Katelyns-MacBook-Air.local>
2026-01-22 13:33:59 -0800

    update

---
+++ 10demo.py
@@ -0,0 +1,16 @@
+# 10demo.py by Katelyn Petersen
+
+import math
+
+print('hello, again')
+print(1.5e-2)
+print(1 + 1)
+print(2**3)
+print(pow(2, 3))
+print(math.pow(2, 3))
+print(2**0.5)
+print(math.sqrt(2))
+print(math.log(2))
+#print(1 / 0)           # divide by zero error
+#print(math.log(0))     # math domain error
+#print(math.sqrt(-1))   # math domain error
