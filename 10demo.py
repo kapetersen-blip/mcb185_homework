b53b27b75f364d4a1cc64af7005e59f37d11c1c9
Katelyn Petersen <katelynpetersen@Katelyns-MacBook-Air.local>
2026-01-22 13:53:26 -0800

    Add shape, conversion, and mean functions

--- 10demo.py
+++ 10demo.py
@@ -19,4 +19,62 @@ def pythagoras(a, b):
 	return c
 hyp = pythagoras(3, 4)
 print(hyp)               
+import math
+
+# Circle area
+def circle_area(r):
+    return math.pi * r**2
+
+# Rectangle area
+def rectangle_area(w, h):
+    return w * h
+
+# Triangle area using rectangle_area
+def triangle_area(w, h):
+    return rectangle_area(w, h) / 2
+
+# Circle circumference
+def circle_circumference(r):
+    return 2 * math.pi * r
+
+# Fahrenheit to Celsius
+def f_to_c(f):
+    return (f - 32) * 5 / 9
+
+# Celsius to Fahrenheit
+def c_to_f(c):
+    return c * 9 / 5 + 32
+
+# MPH to KPH
+def mph_to_kph(mph):
+    return mph * 1.60934
+
+# KPH to MPH
+def kph_to_mph(kph):
+    return kph / 1.60934
+
+# Compute DNA concentration from OD260
+# Assume standard: 1 OD260 unit = 50 ug/mL for dsDNA
+def dna_concentration(od260):
+    return od260 * 50
+
+# Arithmetic mean of 3 numbers
+def arithmetic_mean(a, b, c):
+    return (a + b + c) / 3
+
+# Geometric mean of 3 numbers
+def geometric_mean(a, b, c):
+    return (a * b * c) ** (1/3)
+
+# Harmonic mean of 3 numbers
+def harmonic_mean(a, b, c):
+    return 3 / (1/a + 1/b + 1/c)
+
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+print("Circle area r=2:", circle_area(2))
+print("Triangle area 3x4:", triangle_area(3,4))
+print("100 F in C:", f_to_c(100))
+print("Distance between (0,0) and (3,4):", distance(0,0,3,4))
 
