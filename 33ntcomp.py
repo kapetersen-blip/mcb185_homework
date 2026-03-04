feb778807345e9d331410dac1741869f97b48295
Katelyn Petersen <katelynpetersen@Katelyns-MacBook-Air.local>
2026-03-03 18:09:05 -0800

    Homework problems

--- 33ntcomp.py
+++ 33ntcomp.py
@@ -1,14 +1,14 @@
-1    import sys
-2    import mcb185
-3
-4    for defline, seq in mcb185.read_fasta(sys.argv[1]):
-5        defwords = defline.split()
-6        name = defwords[0]
-7	nts = 'ACGTN'
-8   counts = [0] * len(nts)
-9   for nt in seq:
-10     idx = nts.find(nt)
-11     counts[idx] += 1
-12  print(name, end=' ')
-13  for n in counts: print(n/len(seq), end=' ')
-14  print()
+   import sys
+   import mcb185
+   
+for defline, seq in mcb185.read_fasta(sys.argv[1]):
+     defwords = defline.split()
+     name = defwords[0]
+     nts = 'ACGTN'
+	  counts = [0] * len(nts)
+for nt in seq:
+  	 idx = nts.find(nt)
+     counts[idx] += 1
+     print(name, end=' ')
+     for n in counts: print(n/len(seq), end=' ')
+print()
